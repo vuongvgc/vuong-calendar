100c06f83eace0b26858ab7fb771584819ec95d4
Vuong Do <vuongvgc@gmail.com>
2021-12-04 19:33:40 +0700

    Feature - add Calendar component

--- src/view/Homepage/index.tsx
+++ src/view/Homepage/index.tsx
@@ -1,11 +1,15 @@
+import { Row, Col } from "antd";
 import React from "react";
 import MainCalendar from "../MainCalendar";
 
 const Homepage: React.FC = () => {
   return (
-    <div>
-      <MainCalendar />
-    </div>
+    <Row className="homepage">
+      <Col span={6} className="left-calendar__box"></Col>
+      <Col span={14} className="right-calendar__box">
+        <MainCalendar />
+      </Col>
+    </Row>
   );
 };
 export default Homepage;
--- src/view/MainCalendar/index.tsx
+++ src/view/MainCalendar/index.tsx
@@ -1,6 +1,33 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import React from "react";
 
 const MainCalendar: React.FC = () => {
-  return <div>Main Calendar</div>;
+  const handleDateClick = (arg: any) => {
+    console.debug("arg", arg);
+  };
+  const renderEventContent = (eventInfo: any) => {
+    console.debug("eventInfo", eventInfo);
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      events={[
+        { title: "event 1", date: "2021-12-01" },
+        { title: "event 2", date: "2021-12-02" },
+      ]}
+      dateClick={(arg: any) => handleDateClick(arg)}
+      eventContent={renderEventContent}
+    />
+  );
 };
 export default MainCalendar;
