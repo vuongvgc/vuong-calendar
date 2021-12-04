b3f7c878bec03462634d2423ea5c42d0fa685bb6
Vuong Do <vuongvgc@gmail.com>
2021-12-04 23:04:52 +0700

    Feature- add feature for calendar

--- src/view/MainCalendar/index.tsx
+++ src/view/MainCalendar/index.tsx
@@ -1,4 +1,7 @@
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar, {
+  EventClickArg,
+  EventHoveringArg,
+} from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import React from "react";
@@ -16,17 +19,95 @@ const MainCalendar: React.FC = () => {
       </>
     );
   };
+  let eventData = [
+    {
+      events: [
+        {
+          title: "event1",
+          date: "2022-01-01",
+          dataEvent: {
+            location: "New York",
+            user: "Victor",
+            note: "Play game",
+          },
+        },
+        {
+          title: "event2",
+          date: "2022-01-01",
+          dataEvent: {
+            location: "New York",
+            user: "Victor",
+            note: "Play game",
+          },
+        },
+        {
+          title: "event3",
+          date: "2022-01-01",
+          dataEvent: {
+            location: "New York",
+            user: "Victor",
+            note: "Play game",
+          },
+        },
+        {
+          title: "event2",
+          date: "2022-01-05",
+        },
+        {
+          title: "event3",
+          date: "2022-01-05",
+        },
+      ],
+      color: "#5684AE;",
+      textColor: "#000000;",
+    },
+    {
+      events: [
+        {
+          title: "event1",
+          start: "2021-01-01",
+        },
+        {
+          title: "event2",
+          start: "2021-12-05",
+          end: "2021-12-07",
+        },
+        {
+          title: "event3",
+          date: "2021-12-09",
+        },
+      ],
+      color: "#F9BE81", // an option!
+      textColor: "#0F4C81", // an option!
+    },
+  ];
+  const handleEvent = (info: EventClickArg) => {
+    console.debug("infor", info);
+  };
 
   return (
     <FullCalendar
       plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
-      events={[
-        { title: "event 1", date: "2021-12-01" },
-        { title: "event 2", date: "2021-12-02" },
-      ]}
+      eventSources={eventData}
       dateClick={(arg: any) => handleDateClick(arg)}
       eventContent={renderEventContent}
+      headerToolbar={{
+        start: "today prev,next",
+        center: "title",
+        end: "myCustomButton",
+      }}
+      customButtons={{
+        myCustomButton: {
+          text: "Month",
+          click: function () {
+            alert("clicked the custom button!");
+          },
+        },
+      }}
+      eventClick={(info) => handleEvent(info)}
+      eventMouseEnter={(info: EventHoveringArg) => console.debug("hover", info)}
+      dayMaxEvents={true}
     />
   );
 };
