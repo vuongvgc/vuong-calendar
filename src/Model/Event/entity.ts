863c9694b3cfec56ce491cf4de0492aebf8ff358
Vuong Do <vuongvgc@gmail.com>
2021-12-08 08:56:48 +0700

    Feature- calendar- pass data event to calendar

--- src/Model/Event/entity.ts
+++ src/Model/Event/entity.ts
@@ -4,27 +4,21 @@ class EventEntity {
   id: string | undefined;
   title: string | undefined;
   description?: string | undefined;
-  eventType: string | undefined;
+  eventType: number | undefined;
   url?: string;
   date?: string;
   start?: string;
   end?: string;
-  startTime?: string;
-  endTime?: string;
   constructor(event: any) {
     if (!event) return;
     Object.assign(this, event);
     this.start = event.createdAt
       ? moment(event.start).format("YYYY-MM-DD")
-      : "";
-    this.end = event.end ? moment(event.end).format("YYYY-MM-DD") : "";
-    this.date = event.date ? moment(event.date).format("YYYY-MM-DD") : "";
-    this.startTime = event.startTime
-      ? moment(event.startTime).format("HH:MM:SS")
-      : "";
-    this.endTime = event.endTime
-      ? moment(event.endTime).format("HH:MM:SS")
-      : "";
+      : undefined;
+    this.end = event.end ? moment(event.end).format("YYYY-MM-DD") : undefined;
+    this.date = event.date
+      ? moment(event.date).format("YYYY-MM-DD")
+      : undefined;
   }
   static createListEvent(listEvent: Array<any>) {
     if (!Array.isArray(listEvent)) return [];
@@ -35,26 +29,31 @@ class EventEntity {
   static createListEventForCalendar(listEvent: Array<any>) {
     if (!Array.isArray(listEvent)) return [];
     console.debug("list", listEvent);
-    let eventNormal = listEvent.filter(
-      (item: EventEntity) => item.eventType === "0"
+    let createEventList = EventEntity.createListEvent(listEvent);
+    createEventList.forEach((item: any) => {
+      Object.keys(item).forEach(
+        (key) => item[key] === undefined && delete item[key]
+      );
+    });
+    let eventNormal = createEventList.filter(
+      (item: EventEntity) => item.eventType === 0
     );
-
-    let eventWebinar = listEvent.filter(
-      (item: EventEntity) => item.eventType === "1"
+    let eventWebinar = createEventList.filter(
+      (item: EventEntity) => item.eventType === 1
     );
     console.debug("eventNormal", eventNormal, eventWebinar);
     let newEventArray = [
       {
-        events: EventEntity.createListEvent(eventNormal),
+        events: eventNormal,
         textColor: "#5684AE",
         backgroundColor: "#f7d6b8",
         borderColor: "#0F4C81",
         eventType: 0,
       },
       {
-        events: EventEntity.createListEvent(eventWebinar),
-        color: "#F9BE81", // an option!
-        textColor: "#5684AE", // an option!
+        events: eventWebinar,
+        color: "#F9BE81",
+        textColor: "#5684AE",
         borderColor: "#5684AE",
         eventType: 1,
       },
--- src/view/MainCalendar/index.tsx
+++ src/view/MainCalendar/index.tsx
@@ -39,82 +39,6 @@ const MainCalendar: React.FC = () => {
       </div>
     );
   };
-  let eventData = [
-    {
-      events: [
-        {
-          title: "First section with Alex Stone",
-          date: "2022-01-01",
-          url: "",
-          id: "1",
-        },
-        {
-          id: "2",
-          title: "Watch Football Mu-Chelsea",
-          url: "https://www.google.com/",
-          date: "2022-01-01",
-        },
-        {
-          id: "3",
-          title: "Go to Ho Chi Minh City",
-          url: "",
-          date: "2021-12-12",
-        },
-        {
-          id: "4",
-          title: "See Movie",
-          url: "",
-          date: "2021-12-1",
-        },
-        {
-          id: "11",
-          title: "Go to market",
-          date: "2021-12-12",
-          dataEvent: {
-            location: "Coop mart Long An",
-            description: "Go with family",
-          },
-        },
-      ],
-      textColor: "#5684AE",
-      backgroundColor: "#f7d6b8",
-      borderColor: "#0F4C81",
-    },
-    {
-      events: [
-        {
-          id: "5",
-          title: "Party with friend",
-          start: "2021-12-01",
-        },
-        {
-          id: "6",
-          title: "Go business trip",
-          start: "2021-12-05",
-          end: "2021-12-15",
-        },
-        {
-          id: "7",
-          title: "Birthday's Girl Friend",
-          date: "2021-12-12",
-        },
-        {
-          id: "8",
-          title: "Buy birthday gift",
-          date: "2021-12-11",
-        },
-        {
-          id: "9",
-          title: "Go to the restaurant",
-          date: "2021-12-11",
-          description: "Go with Girl friend",
-        },
-      ],
-      color: "#F9BE81", // an option!
-      textColor: "#5684AE", // an option!
-      borderColor: "#5684AE",
-    },
-  ];
   const handleEvent = (info: EventClickArg) => {
     // console.debug("infor", info);
     if (!info.event.url) {
@@ -126,7 +50,7 @@ const MainCalendar: React.FC = () => {
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        eventSources={eventData}
+        eventSources={dataCalendar}
         dateClick={(arg: any) => handleDateClick(arg)}
         eventContent={renderEventContent}
         headerToolbar={{
